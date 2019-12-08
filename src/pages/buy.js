2d1da235efc6019dcc42161b0575c240eff2ade2
MilanBarande <milan.barande@hotmail.fr>
2019-12-08 12:06:35 +0100

    :sparkles: Add buy page

---
+++ src/pages/buy.js
@@ -0,0 +1,15 @@
+import React from "react"
+import LinkList from "../components/linklist"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "./buy.css"
+
+const BuyPage = () => (
+  <Layout>
+    <SEO title="Buy" />
+    <LinkList />
+  </Layout>
+)
+
+export default BuyPage
