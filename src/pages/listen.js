a1e427645082aeaa220fcd900b9016fef9ae7cf5
MilanBarande <milan.barande@hotmail.fr>
2020-01-04 15:17:45 +0100

    :sparkles: Only hide links for mobile view on listen page

--- src/pages/listen.js
+++ src/pages/listen.js
@@ -1,22 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import LinkList from "../components/linklist"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import "./listen.css"
 
-const ListenPage = () => (
-  <Layout>
-    <SEO title="Listen" />
-    <iframe
-      width="80%"
-      height="600"
-      scrolling="no"
-      frameborder="no"
-      allow="autoplay"
-      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/874459783&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-    ></iframe>
-  </Layout>
-)
+const ListenPage = () =>
+  window ? (
+    <Layout>
+      <SEO title="Listen" />
+      <iframe
+        width="80%"
+        height="600"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/874459783&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      ></iframe>
+      {window.innerWidth > 800 && <LinkList />}
+    </Layout>
+  ) : null
 
 export default ListenPage
