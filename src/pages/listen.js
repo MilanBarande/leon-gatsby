805094d60eb3fcf8025d00fcc7dd0583b81f617a
MilanBarande <milan.barande@hotmail.fr>
2020-01-04 15:46:18 +0100

    :recycle:

--- src/pages/listen.js
+++ src/pages/listen.js
@@ -5,20 +5,19 @@ import SEO from "../components/seo"
 
 import "./listen.css"
 
-const ListenPage = () =>
-  window ? (
-    <Layout>
-      <SEO title="Listen" />
-      <iframe
-        width="80%"
-        height="600"
-        scrolling="no"
-        frameBorder="no"
-        allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/874459783&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-      ></iframe>
-      {window.innerWidth > 800 && <LinkList />}
-    </Layout>
-  ) : null
+const ListenPage = () => (
+  <Layout>
+    <SEO title="Listen" />
+    <iframe
+      width="80%"
+      height="600"
+      scrolling="no"
+      frameBorder="no"
+      allow="autoplay"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/874459783&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+    ></iframe>
+    <LinkList />
+  </Layout>
+)
 
 export default ListenPage
