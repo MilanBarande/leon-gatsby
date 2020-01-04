88b6eca99836c0324e283cdee35e2298a9f47925
MilanBarande <milan.barande@hotmail.fr>
2020-01-04 15:51:11 +0100

    :sparkles: About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import LinkList from "../components/linklist"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import "./about.css"
 
-const AboutPage = () => (
-  <Layout>
-    <SEO title="About" />
-    <LinkList />
-  </Layout>
-)
+const bio = {
+  french:
+    "Léon c’est l’artiste complet, le touche à tout de talent, musicien, chanteur, danseur, perfomer… son concert « project yourself » est une véritable pépite à 360°. Sa voix, sa présence scénique, sa musique, son énergie rythmique électrisent. Aujourd’hui basé à Berlin, ce grand créatif propose une électro-pop qui puise ses influences dans la new wave, le jazz et la techno. Le voir en concert c’est plonger dans un véritable univers pop, élégant, incarné et singulier… une véritable expérience live.",
+  english:
+    "Singer/Songwritter, Dancer and Performer, Léon creates Electronic/Pop music with influences from 80's-New Wave, Jazz and Techno aesthetics. The young artist graduated with a Dance and Choreographic Bachelor at the infamous Folkwang University in Essen Germany and is now based in Berlin. Live - Léon plays solo, an interdisciplinary concert incorporating singing, electro music, dance, cabaret and theatrical elements that will invite you to trust in what you stand for - BE YOURSELF and do not wait for anyones approval.",
+}
+
+const AboutPage = () => {
+  const [language, setLanguage] = useState("english")
+  const toggleLanguage = () =>
+    language === "english" ? setLanguage("french") : setLanguage("english")
+  return (
+    <Layout>
+      <SEO title="About" />
+      <div className="about-text">{bio[language]}</div>
+      <div className="toggle-language-button" onClick={toggleLanguage}>
+        {language === "english" ? "🇫🇷" : "🇬🇧 🇺🇸"}
+      </div>
+      <LinkList />
+    </Layout>
+  )
+}
 
 export default AboutPage
