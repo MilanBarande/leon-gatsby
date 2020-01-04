796a34d6d0ff5c73a80cd3c6cae944b53ed44312
MilanBarande <milan.barande@hotmail.fr>
2020-01-04 15:04:58 +0100

    :sparkles: Add soundcloud player

--- src/pages/listen.js
+++ src/pages/listen.js
@@ -8,7 +8,14 @@ import "./listen.css"
 const ListenPage = () => (
   <Layout>
     <SEO title="Listen" />
-    <LinkList />
+    <iframe
+      width="80%"
+      height="600"
+      scrolling="no"
+      frameborder="no"
+      allow="autoplay"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/874459783&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+    ></iframe>
   </Layout>
 )
 
