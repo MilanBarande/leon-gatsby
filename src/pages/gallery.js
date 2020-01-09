f62c11989ed4d51f48735cd1cb3eab0c73c158d9
MilanBarande <milan.barande@hotmail.fr>
2020-01-09 17:18:36 +0100

    Remove warning class --> classNam

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -8,8 +8,8 @@ import "./gallery.scss"
 const GalleryPage = () => (
   <Layout className="gallery">
     <SEO title="Gallery" />
-    <div class="gallery-container">
-      <div class="elfsight-app-e415eb7e-aaa6-4a9f-9806-3d264a39bac7"></div>
+    <div className="gallery-container">
+      <div className="elfsight-app-e415eb7e-aaa6-4a9f-9806-3d264a39bac7"></div>
     </div>
     <LinkList />
   </Layout>
